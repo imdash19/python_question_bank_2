815d6bb5e1405a4bb9369d5b8db089c461748f26
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-06-30 19:42:12 +0530

    Add LMS class with static welcome message method

---
+++ lms_static_welcome.py
@@ -0,0 +1,8 @@
+# Create class LMS with static method welcome_message that prints welcome text
+
+class LMS:
+    @staticmethod
+    def welcome_message():
+        print("Welcome to LMS")
+
+LMS.welcome_message()
