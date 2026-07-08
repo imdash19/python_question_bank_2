be7d25e7b724d9d5f8dbfce4d661ba16fec6452c
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-08 16:29:25 +0530

    Update comments and add list input functionality

---
+++ shop_transactions_list.py
@@ -0,0 +1,5 @@
+# A shop records the number of items sold in five transactions. 
+# Take five numbers as input and store them in a list. Print the list.
+
+lst= [int(val) for val in input().split()]
+print(lst)
