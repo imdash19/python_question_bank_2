114e67cc9f1e30d64e7d336ce49bbcceb47975f7
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-08 16:30:42 +0530

    Refactor user registration script with input handling
    Updated comments and added input handling for name and age.

---
+++ user_registration_list.py
@@ -0,0 +1,6 @@
+# A user registration system stores name and age together. 
+# Take name and age as input and store them in a list. Print the list.
+
+name, age= map(input().split())
+lst= [name, age]
+print(lst)
