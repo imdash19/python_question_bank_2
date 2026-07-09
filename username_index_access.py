f0c58075d0d5974160b72f11fc1d7e7bea820fd5
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-09 20:49:25 +0530

    Add comments and improve code formatting

---
+++ username_index_access.py
@@ -0,0 +1,6 @@
+# A system stores usernames ['user1','user2','user3']. 
+# Take an index as input and display the username at that position.
+
+user= ['user1','user2','user3']
+idx= int(input())
+print(user[idx])
