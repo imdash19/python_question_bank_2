85e260f67e5536dd2b252e8b614684e1342dc50e
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-09 20:46:59 +0530

    Update comments and fix list initialization
    Fixed comment formatting and corrected list initialization syntax.

---
+++ weather_readings_list.py
@@ -0,0 +1,6 @@
+# A weather station records three temperature readings. 
+# Take three numbers as input and store them in a list. 
+# Print the readings.
+
+lst= list(map(int, input().split()]
+print(lst)
