e4488226d1eb7b984c3215876efb74c8a71ff737
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-09 20:50:24 +0530

    Comment out initial description in product_price_index.py

---
+++ product_price_index.py
@@ -0,0 +1,6 @@
+# A product catalog stores prices [100,200,300,400]. 
+# Take an index as input and display the price at that position.
+
+price= [100,200,300,400]
+idx= int(input())
+print(price[idx])
