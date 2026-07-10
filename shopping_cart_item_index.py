d1f8119f4196677435589971c01b6dc2354b41b5
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-10 19:44:55 +0530

    Add cart list and input handling for item display

---
+++ shopping_cart_item_index.py
@@ -0,0 +1,8 @@
+# A shopping cart contains ['Pen','Book','Bag','Mouse']. 
+# Take an index and display the item at that position.
+
+cart = ["Pen", "Book", "Bag", "Mouse"]
+
+index = int(input())
+
+print(cart[index])
