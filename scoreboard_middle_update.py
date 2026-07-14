b640771a12b96d2a512c7dc8d3888c75b3cabecd
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-14 18:51:29 +0530

    Update comments and add input for middle score

---
+++ scoreboard_middle_update.py
@@ -0,0 +1,8 @@
+# A scoreboard contains [10,20,30]. 
+# The middle score needs correction. 
+# Take a new value and update the middle element. 
+# Print the list.
+
+lst= [10, 20, 30]
+lst[1]= int(input())
+print(lst)
