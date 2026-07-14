d3ee164bebc91a5dd4801fb8eb14a988d9f1ecab
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-14 18:52:09 +0530

    Refactor student list update script
    Updated comments and changed string quotes to double quotes.

---
+++ student_list_last_update.py
@@ -0,0 +1,12 @@
+# A list of students is ['Ravi','Anu','Sam']. 
+# The last student has dropped out. 
+# Take a new name and replace the last element. 
+# Print the updated list.
+
+students = ["Ravi", "Anu", "Sam"]
+
+new_name = input()
+
+students[-1] = new_name
+
+print(students)
