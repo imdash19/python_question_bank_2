299b3e11c98efa5aefa5d29829ffded14cdae383
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-15 17:11:23 +0530

    Refactor comments and add newline in scoreboard_remove_score.py
    Updated comments to be active code lines and added a newline.

---
+++ scoreboard_remove_score.py
@@ -0,0 +1,11 @@
+# A scoreboard contains [10,20,30,40]. 
+# Take a score value as input and remove it from the list. 
+# Print updated scores.
+
+scores = [10, 20, 30, 40]
+
+score = int(input())
+
+scores.remove(score)
+
+print(scores)
