10d5c726c443b722af9f68d46b19c1b6df1054b3
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-15 17:10:43 +0530

    Refactor cart removal code with proper implementation

---
+++ cart_remove_by_index.py
@@ -0,0 +1,11 @@
+# A cart contains ['Pen','Book','Bag']. 
+# Take an index as input and remove the item at that position. 
+# Print updated cart.
+
+cart = ["Pen", "Book", "Bag"]
+
+index = int(input())
+
+cart.pop(index)
+
+print(cart)
