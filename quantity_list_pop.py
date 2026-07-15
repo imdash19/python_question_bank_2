26d4855b2664c83ffa05f9f616eb3da546c1e767
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-15 17:11:58 +0530

    Refactor comments and implement pop functionality

---
+++ quantity_list_pop.py
@@ -0,0 +1,11 @@
+# A list [5,10,15,20] stores quantities. 
+# Take an index and remove the element using pop. 
+# Print updated list.
+
+quantities = [5, 10, 15, 20]
+
+index = int(input())
+
+quantities.pop(index)
+
+print(quantities)
