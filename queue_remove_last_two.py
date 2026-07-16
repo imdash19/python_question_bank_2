289ae2bf842275fd48f052ab9750f1d441271451
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-16 18:48:45 +0530

    Add comments and remove last two elements from queue

---
+++ queue_remove_last_two.py
@@ -0,0 +1,6 @@
+# A queue ['A','B','C','D'] contains last two customers who canceled. 
+# Remove the last two elements and print updated queue.
+
+queue= ['A','B','C','D']
+del queue[-2:]
+print(queue)
