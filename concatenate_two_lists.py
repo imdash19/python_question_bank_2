e11481729294637dd3ea1d17d88cbca45315c226
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-18 16:13:11 +0530

    Correct list concatenation and print variable
    Fix variable name in print statement and update list concatenation.

---
+++ concatenate_two_lists.py
@@ -0,0 +1,8 @@
+# Take two lists of two numbers each from input and concatenate them. 
+# Print the final list.
+
+lst1= [int(val) for val in input().split()]
+lst2= [int(val) for val in input().split()]
+
+lst1+= lst2
+print(lst3)
