5c40bc433b89c38ad761424bc6952863fd0e519a
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-18 16:11:13 +0530

    Refactor comments and add input handling for temperatures

---
+++ weather_readings_nested_list.py
@@ -0,0 +1,6 @@
+# A weather system stores each temperature as a separate record. 
+# Take three readings and store them as [[temp1],[temp2],[temp3]]. 
+# Print the structure.
+
+temp= [[int(val)] for val in input().split()]
+print(temp)
