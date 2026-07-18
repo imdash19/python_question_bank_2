7ec48c613fbb62ddd5669153fe2272068e9000cb
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-18 16:15:25 +0530

    Add input functionality to merge employee teams

---
+++ merge_employee_teams.py
@@ -0,0 +1,8 @@
+# Take two employee names for team A and two for team B. 
+# Merge both teams into one list.
+
+lst1= [input(), input()]
+lst2= [input(), input()]
+
+merged_list= lst1+lst2
+print(merged_list)
