735451dd48f6314c49e4f60af19c2c290a516325
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-20 16:45:11 +0530

    Fix variable name and add comments in combine_subject_sessions.py

---
+++ combine_subject_sessions.py
@@ -0,0 +1,8 @@
+# Take two subjects from morning session and two from afternoon session. 
+# Combine them into one list.
+
+s1= list(input().split())
+s2= list(input().split())
+
+sesions= s1+s2
+print(sessions)
