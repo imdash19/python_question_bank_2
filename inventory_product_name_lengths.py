3555397fb445dd0f832f05f8a8e82cf6b5f3c07a
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-23 19:59:38 +0530

    Refactor comments and add product name length calculation

---
+++ inventory_product_name_lengths.py
@@ -0,0 +1,5 @@
+# An inventory system needs the length of product names. 
+# Take two product names and generate a list containing their lengths.
+
+product_name_length= [len(val) for val in input().split()]
+print(product_name_length)
