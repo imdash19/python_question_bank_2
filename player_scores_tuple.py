150f5b79b1841247d7c01480b8eab4ea0b2811c2
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-24 20:10:23 +0530

    Add comments and tuple input for player scores

---
+++ player_scores_tuple.py
@@ -0,0 +1,5 @@
+# A game records three scores of a player. 
+# Take three scores as input, store them in a tuple, and print it.
+
+records= tuple([int(input()), int(input()), int(input())])
+print(records)
