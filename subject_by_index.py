1f8ba8551db563d5faf4ba44dd65c0d7b0339500
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-25 20:02:49 +0530

    Add subject tuple and index input functionality

---
+++ subject_by_index.py
@@ -0,0 +1,6 @@
+# A tuple ('Math','Science','English','History') stores subjects. 
+# Take an index and print the subject.
+
+subject= ('Math','Science','English','History')
+idx= int(input())
+print(subject[idx])
