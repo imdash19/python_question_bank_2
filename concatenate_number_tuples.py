a73b7132b67c79469a24a41de6aa9ab7a08b00bf
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-26 19:38:00 +0530

    Add tuple concatenation functionality

---
+++ concatenate_number_tuples.py
@@ -0,0 +1,7 @@
+# Take two tuples of two numbers each from input and concatenate them. 
+# Print the final tuple.
+
+t1= (int(input()), int(input()))
+t2= (int(input()), int(input()))
+t3= t1 + t2
+print(t3)
