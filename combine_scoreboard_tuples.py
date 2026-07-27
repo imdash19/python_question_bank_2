3158645f57c23b5bf671d7c1a50fa46a03895f86
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-27 15:56:57 +0530

    Fix comments and combine scoreboard tuples

---
+++ combine_scoreboard_tuples.py
@@ -0,0 +1,8 @@
+# Take two scores for first game and two for second game. 
+# Combine them into one scoreboard tuple.
+
+s1= (int(input()), int(input()))
+s2= (int(input()), int(input()))
+
+s3= s1+s2
+print(s3)
