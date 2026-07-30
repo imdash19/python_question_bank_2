a2f993a9111b98f0a27aec4613d028697d49cd25
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-30 22:19:28 +0530

    Refactor string identity check to compare values
    Updated the string identity check program to read two strings and compare their values instead of their identity.

---
+++ string_identity_check.py
@@ -0,0 +1,8 @@
+# Write a Python program to read two strings with the same text.
+# Use the is operator to compare both strings.
+# Check whether Python stores them in the same memory location.
+# If both refer to the same object, print True.
+# Otherwise, print False.
+
+s1, s2= input(), input()
+print(s1 == s2)
