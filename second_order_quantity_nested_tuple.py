228fbefe3b051aae836c0ae37de3c2f536015913
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-30 22:04:38 +0530

    Implement quantity printing for order tuple
    Added code to print quantities from order tuple.

---
+++ second_order_quantity_nested_tuple.py
@@ -0,0 +1,6 @@
+# An order tuple (('Pen',2),('Book',3)) stores order details. 
+# Access and print the quantity of the second order.
+
+store= (('Pen',2),('Book',3))
+for val in store:
+  print(val[1])
