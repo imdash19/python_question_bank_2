03d2932dc0f72ed4331335502a42a08dbb2bf474
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-30 22:20:43 +0530

    Refactor numeric_identity_is_not.py for clarity

---
+++ numeric_identity_is_not.py
@@ -0,0 +1,8 @@
+# Write a Python program to read two numeric values.
+# Use the is not operator to compare the variables.
+# Check whether both variables refer to different objects.
+# If they are different objects, print True.
+# Otherwise, print False.
+
+n1, n2= int(input()), int(input())
+print(n1 is n2)
