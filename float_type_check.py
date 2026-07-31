9483b6ed3ec092bf39001333a16c9cebf62cfd41
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-31 11:13:38 +0530

    Refactor float_type_check.py to include input handling

---
+++ float_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to store a decimal number in a variable.
+# Use the type() function to identify its data type.
+# Observe how Python recognizes floating-point values.
+# Print the detected data type on the screen.
+
+n= float(input())
+print(type(n))
