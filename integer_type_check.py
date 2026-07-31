a416a380ea71c88514da1ec715d58e3dacc09ea0
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-07-31 11:12:47 +0530

    Convert comments to code in integer_type_check.py

---
+++ integer_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to store a whole number in a variable.
+# Use the type() function to check the data type of the variable.
+# Understand how Python classifies integer values.
+# Display the result showing that the value belongs to the integer type.
+
+n= int(input())
+print(type(n))
