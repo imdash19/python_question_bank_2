670a8cb722d11d21413faf6cd4db5d5cf0ca7a1d
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-01 07:32:22 +0530

    Convert comments to code in string_type_check.py

---
+++ string_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to assign a text value to a variable.
+# Use the type() function to check the variable’s data type.
+# Understand how Python treats text as a string.
+# Print the output showing the string data type.
+
+s= input()
+print(type(s))
