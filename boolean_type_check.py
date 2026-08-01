7b2784e5f2bc18e421921efe65d5f3b7cf989be8
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-01 07:33:48 +0530

    Comment out instructions and add Boolean example

---
+++ boolean_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to store a Boolean value (True or False).
+# Use the type() function to determine the data type.
+# Learn how Python handles Boolean values internally.
+# Display the Boolean type as the output.
+
+val= True
+print(type(val))
