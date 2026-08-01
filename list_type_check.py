24691cf67c021a26197da9c2ff9121fad88c9e26
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-01 07:35:03 +0530

    Update comments and add list creation example

---
+++ list_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to create a list with three elements.
+# Store numbers or strings inside the list.
+# Use the type() function to check the data type.
+# Print the result to see how Python identifies lists.
+
+lst= [19, 'Bibhuti', 95.9]
+print(type(lst))
