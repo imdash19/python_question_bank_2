3c81d603638e371c0495b89568fadf313643df2b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-01 07:38:25 +0530

    Fix addition of two integers in add_two_integers.py
    Corrected the code to properly add two integers and print the result.

---
+++ add_two_integers.py
@@ -0,0 +1,7 @@
+# Write a Python program to accept two whole numbers as input.
+# Store both values in separate variables.
+# Add the two integer values using the addition operator.
+# Display the final sum as output.
+
+n1, n2= int(input()), int(input())
+print(s1+s2)
