0f7c59725de24270f110966db7d15b8a05e59969
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-02 20:24:32 +0530

    Fix variable name in type check print statement
    Corrected variable name in print statement to reflect input variable.

---
+++ float_input_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to accept a float number as input.
+# Store the value in a variable.
+# Use the type() function to identify its data type.
+# Print the result to confirm it is a float type.
+
+n= float(input())
+print(type(f))
