67a2ec387cf2acb379c1309e6ce59e39e483aac7
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-02 20:28:26 +0530

    Refactor student details program with f-strings

---
+++ student_details_formatting.py
@@ -0,0 +1,8 @@
+# Write a Python program to store name, age, and marks in variables.
+# Create a meaningful sentence using these variables.
+# Use f-strings or the format() method for formatting.
+# Combine text and variable values neatly.
+# Print the formatted output.
+
+name, age, marks= input(), int(input()), float(input())
+print(f'Hi! My name is {name}, I'm {age} years old & I've secured {marks} in my degree')
