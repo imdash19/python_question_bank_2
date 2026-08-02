b45f94f71bf08078e9c349a56c51fac979cd3a2a
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-02 20:29:29 +0530

    Implement number list creation and printing

---
+++ create_number_list.py
@@ -0,0 +1,7 @@
+# Write a Python program to create a list containing 5 numbers: 10 20 30 40 50.
+# Store the numbers in a variable as a list.
+# Understand how lists are defined in Python.
+# Print the list to display its contents.
+
+lst= [10, 20, 30, 40, 50]
+print(lst)
