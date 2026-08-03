e673df442dd88641ecf2748689312cf2f0743cfd
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-03 20:06:26 +0530

    Add tuple creation and type checking example

---
+++ mixed_tuple_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to create a tuple with different types of values.
+# Include integers, strings, and Boolean values in the tuple.
+# Use the type() function to check the tuple’s data type.
+# Print the type to verify Python identifies it correctly.
+
+tup= ('Bibhuti', 19, 23.1)
+print(type(tup))
