88d9b7d514f80670666ef9e5e56d6eee8c0205e6
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-03 20:04:20 +0530

    Comment out instructions and add example list

---
+++ mixed_list_type_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to create a list with different types of values.
+# Include numbers, strings, and decimals in the same list.
+# Use the type() function to check the data type of the list.
+# Print the type to see that Python identifies it as a list.
+
+lst= ['Bibhuti', 19, 23,1]
+print(type(lst))
