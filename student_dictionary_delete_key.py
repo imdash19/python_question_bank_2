fd69c92e112433ad0c960aab257e79e67991e091
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-04 21:07:17 +0530

    Update student dictionary and fix key deletion
    Corrected the student name and fixed the deletion syntax.

---
+++ student_dictionary_delete_key.py
@@ -0,0 +1,9 @@
+# Write a Python program to create a dictionary format which represent the student name=Naresh and age=45 in key-value format.
+# First print the dictionary and then Remove the age key and its corresponding value from the dictionary.
+# Update the dictionary after deletion.
+# Print the modified dictionary As shown in test case.
+
+student= {'name': 'Bibhuti', 'age': 23}
+print(student)
+del student[age]
+print(student)
