6d8893566a4a743dc56873f1a527f8cd4afddd43
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-04 22:00:23 +0530

    Refactor comments in set_add_element.py
    Updated comments in set_add_element.py to be active code.

---
+++ set_add_element.py
@@ -0,0 +1,8 @@
+# Write a Python program to create a set with some elements.
+# Use the add() method to insert a new element into the set.
+# Ensure that the element is added without affecting existing elements.
+# Print the updated set.
+
+s= {1, 2, 3, 4}
+s.add(5)
+print(s)
