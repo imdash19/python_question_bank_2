c33e6c4c5a531f7db03539b0b03eac8f72747a20
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-04 21:59:13 +0530

    Refactor comments and implement list to set conversion

---
+++ list_to_set_conversion.py
@@ -0,0 +1,8 @@
+# Write a Python program to create a list containing repeated elements.
+# Convert the list into a set using the set() function.
+# Observe that duplicate values are automatically removed.
+# Print the resulting set.
+
+lst= [1, 2, 3, 2, 3, 4, 4]
+lst= set(lst)
+print(lst)
