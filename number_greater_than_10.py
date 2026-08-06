6531d53304ea7d93832062a9a9bc5962e118a55b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-06 11:14:04 +0530

    Refactor number_greater_than_10.py with comments

---
+++ number_greater_than_10.py
@@ -0,0 +1,6 @@
+# Write a Python program to read a number from the user.
+# Use an if condition to check whether the number is greater than 10.
+# Print a message indicating whether the number is greater than 10 or not.
+
+n= int(input())
+print('Greater than 10' if n > 10 else 'Lesser than 10')
