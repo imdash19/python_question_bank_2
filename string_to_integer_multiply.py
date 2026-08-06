3173f4ae916c1f12b6f38c6070546ec5d2a532de
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-06 11:15:18 +0530

    Refactor string to integer multiplication program

---
+++ string_to_integer_multiply.py
@@ -0,0 +1,8 @@
+# Write a Python program to a string containing a numeric value as 10.
+# Convert the string into an integer using int().
+# Perform arithmetic operations such as multiplying by 2.
+# Print the result of the calculation.
+
+s= '10'
+s= int(s) * 2
+print(s)
