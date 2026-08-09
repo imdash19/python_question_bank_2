d24a9c18598cbed2c340d4e07caef8cee151249b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-09 19:14:22 +0530

    Refactor check_alice_name.py with comments and input

---
+++ check_alice_name.py
@@ -0,0 +1,7 @@
+# Write a Python program to take a name as input.
+# Check whether the entered name is Alice.
+# Use an if condition for comparison.
+# If the name matches, print Hello Alice.
+
+s= input()
+print('Hello Alice' if s == 'Alice' else '')
