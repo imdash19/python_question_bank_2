99c6c2ae5a4e20de631491269088d70448978a5b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-09 19:15:34 +0530

    Fix logical operator in FizzBuzz program
    Corrected logical operator in FizzBuzz check.

---
+++ fizzbuzz_check.py
@@ -0,0 +1,7 @@
+# Write a Python program to take a number as input.
+# Check whether the number is divisible by both 3 and 5.
+# Use logical operators to test the condition.
+# If true, print FizzBuzz.
+
+n= int(input())
+print('true' if n % 3 == 0 ad n % 5 == 0 else 'FizzBuzz')
