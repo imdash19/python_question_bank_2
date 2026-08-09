e3dd6e6c93d140b3b97e0114e406b899ea2a9131
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-09 19:16:15 +0530

    Refactor URL check program with comments

---
+++ url_com_check.py
@@ -0,0 +1,11 @@
+# Write a Python program to take a URL as input.
+# Check whether the string contains com.
+# Use string searching methods for validation.
+# If found, print Yes, otherwise print No.
+
+url = input()
+
+if "com" in url:
+    print("Yes")
+else:
+    print("No")
