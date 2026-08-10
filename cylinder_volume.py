d655794a5017e5829bcbb2cf14b53ac9f5831724
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-10 21:36:16 +0530

    Implement cylinder volume calculation in Python

---
+++ cylinder_volume.py
@@ -0,0 +1,7 @@
+# Write a Python program to accept radius and height.
+# Calculate volume using Volume = π × r² × h.
+# Store π as 3.14.
+# Display the volume of the cylinder.
+
+r, h= int(input()), int(input())
+print(3.14 * (r**2) * h)
