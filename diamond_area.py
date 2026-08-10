9ca3f978ef4a54ff8d905da534d7503e3d640d8d
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-10 21:31:01 +0530

    Implement diamond area calculation
    Removed comments and added implementation for calculating the area of a diamond.

---
+++ diamond_area.py
@@ -0,0 +1,8 @@
+# Write a Python program to accept diagonals d1 and d2.
+# Calculate the area using ½ × d1 × d2.
+# Print the calculated area.
+# Use clear variable names.
+
+d1, d2= int(input()), int(input())
+area= 1/2 * d1 * d2
+print(area)
