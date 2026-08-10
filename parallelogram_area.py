7a8e4649029d9fa339bda359ad9344bd8c9c9671
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-10 21:34:01 +0530

    Refactor parallelogram_area.py to remove comments

---
+++ parallelogram_area.py
@@ -0,0 +1,7 @@
+# Write a Python program to accept base and height.
+# Calculate the area using base × height.
+# Display the area of the parallelogram.
+# Ensure clarity in output.
+
+b, h= int(input()), int(input())
+print(b*h)
