18fe7ca5f6e51db89989a0bc66339671e314837d
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-10 21:34:59 +0530

    Implement surface area calculation for a cube
    Removed comments and added functional code to calculate the surface area of a cube.

---
+++ cube_surface_area.py
@@ -0,0 +1,7 @@
+# Write a Python program to accept the side of a cube.
+# Calculate the surface area using SA = 6 × side².
+# Print the surface area.
+# Use arithmetic operators only.
+
+s= int(input())
+print(6 * (s**2))
