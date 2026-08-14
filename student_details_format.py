6119962fc4a5ff58dfa4fbd40365510acfb0ba30
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-14 19:29:53 +0530

    Refactor comments and implement student details input

---
+++ student_details_format.py
@@ -0,0 +1,9 @@
+# Write a Python program to store student name and marks.
+# Use the .format() method to insert values into a string.
+# Combine text and variables neatly.
+# Print the formatted output.
+
+name = input()
+marks = int(input())
+
+print("Student {} scored {} marks.".format(name, marks))
