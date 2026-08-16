f0388cca81c29661b688d9a508e49cbeb06b1bb6
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-16 20:11:11 +0530

    Implement countdown from 10 to 1 in Python
    Added implementation for countdown from 10 to 1.

---
+++ countdown_10_to_1.py
@@ -0,0 +1,11 @@
+# Write a Python program to print numbers from 10 down to 1.
+# Initialize a variable with the value 10.
+# Use a while loop to decrease the value in each iteration.
+# Continue the loop until the value becomes 1.
+# Print each number during the countdown.
+
+n= 10
+
+while n > 0:
+    print(n, end= ' ')
+    n-= 1
