49932d1b49fa9237683c22c7cd8589cf1b1c4bed
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-19 20:10:06 +0530

    Refactor string concatenation program in Python

---
+++ concatenate_two_strings.py
@@ -0,0 +1,8 @@
+# Write a Python program to accept two strings from the user.
+# Join both strings together using string concatenation.
+# Add a space between the two strings while joining them.
+# Store the combined result in a variable.
+# Print the final concatenated string clearly.
+
+s1, s2= input(), input()
+print(s1+ ' ' +s2)
