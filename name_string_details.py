07716577d71ed4d2702d49f95b77dd022a1a614b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-19 20:11:41 +0530

    Refactor name_string_details.py for clarity

---
+++ name_string_details.py
@@ -0,0 +1,10 @@
+# Write a Python program to store your name in a string variable.
+# Print the original string value.
+# Use the len() function to find the length of the string.
+# Use the type() function to display the data type.
+# Ensure all outputs are printed in a readable format.
+
+name= input()
+print(f'''Original String: {name}
+Length of String: {len(name)}
+Type: {type(name)}''')
