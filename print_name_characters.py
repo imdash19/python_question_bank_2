3ea4ab680a1c8173343c266c3c208e6cb5d47559
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-19 20:07:39 +0530

    Refactor print_name_characters.py to include input

---
+++ print_name_characters.py
@@ -0,0 +1,10 @@
+# Write a Python program to accept a full name from the user as input.
+# Use a loop to go through each character in the string one by one.
+# Print every character on a separate line.
+# Ensure spaces in the name are also handled correctly.
+# Display the output clearly, with one character per line.
+
+name= input()
+
+for v in name:
+    print(v)
