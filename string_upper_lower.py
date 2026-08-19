bbe9202e1c2fac6c43dbd57c5bbfad72e002151f
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-19 20:09:02 +0530

    Convert string to uppercase and lowercase

---
+++ string_upper_lower.py
@@ -0,0 +1,9 @@
+# Write a Python program to accept a string from the user.
+# Use the .upper() method to convert the string to uppercase.
+# Use the .lower() method to convert the string to lowercase.
+# Print both converted results separately.
+# Ensure the original string is not modified permanently.
+
+s= input()
+print(f'''Uppercase: {s.upper()}
+Lowercase: {s.lower()}''')
