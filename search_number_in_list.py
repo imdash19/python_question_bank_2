f7eb8d7074a1e67ab66cab3c2a37e8c8acd0a1cd
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-21 20:23:18 +0530

    Refactor search program to use list comprehension

---
+++ search_number_in_list.py
@@ -0,0 +1,9 @@
+# Write a Python program to search for a specific number in a list.
+# Accept a list of numbers and a search element from the user.
+# Use a while loop to traverse the list using an index.
+# If the element is found, print “Found” and break the loop.
+# Use the else block to print “Not Found” if the search fails.
+
+lst= list(map(int, input().split()))
+n= int(input())
+print('Found' if n in lst else 'Not Found')
