cd06e6f1c7375754723a7cc6aa52b1953499d3a3
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-23 19:50:26 +0530

    Refactor string_length.py to include implementation

---
+++ string_length.py
@@ -0,0 +1,8 @@
+# Write a Python program to accept a string value.
+# Use the built-in len() function to calculate the length of the string.
+# Ensure that spaces are also counted as characters.
+# Store the length in a variable.
+# Print the final length clearly as output.
+
+s= input()
+print(len(s))
