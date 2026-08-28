d3433d2740f04809b1764250b618f5641a0010ca
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-28 19:47:56 +0530

    Create clear_list.py

---
+++ clear_list.py
@@ -0,0 +1,8 @@
+# Write a Python program to accept a list from the user.
+# Use the clear() method to remove all elements.
+# Store the cleared list.
+# Print the empty list clearly.
+
+lst= list(input().split())
+lst.clear()
+print(lst)
