f76b43476710d7fc7deb92541f9fe2ab8d776043
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-29 20:57:23 +0530

    Implement sentence splitting into words

---
+++ split_sentence.py
@@ -0,0 +1,11 @@
+# Write a Python program to accept a sentence from the user.
+# Use the split() method to break the sentence into words.
+# Store the resulting words in a list.
+# Ensure splitting is done based on spaces.
+# Print the list of words as output.
+
+sentence = input()
+
+words = sentence.split(" ")
+
+print(words)
