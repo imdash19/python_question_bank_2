7f0fbba2f4130d1e2026fc5d8a901c547e02474e
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-31 08:21:00 +0530

    Implement tuple slicing to extract elements
    Refactor code to include tuple slicing functionality.

---
+++ tuple_slicing.py
@@ -0,0 +1,7 @@
+# Write a Python program to extract a portion of a tuple.
+# Use slicing to select elements from index 1 up to 4 (excluding 4).
+# Store the sliced elements in a new tuple.
+# Print the resulting tuple.
+
+tup= tuple(input().split())
+print(tup[1:4])
