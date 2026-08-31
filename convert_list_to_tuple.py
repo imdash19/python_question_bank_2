bafaa1f8dbf5b00f9bce295078cacfdae1f4aa0c
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-08-31 08:21:59 +0530

    Implement list to tuple conversion in Python

---
+++ convert_list_to_tuple.py
@@ -0,0 +1,11 @@
+# Write a Python program to take [1,"hello",True,3.14] in a list and convert it into a tuple.
+# Use the tuple() constructor to perform the conversion.
+# Ensure all elements of the list are preserved in the tuple.
+# Print the list and final tuple.
+
+my_list = [1, "hello", True, 3.14]
+
+my_tuple = tuple(my_list)
+
+print(my_list)
+print(my_tuple)
