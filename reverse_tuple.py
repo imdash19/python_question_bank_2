3f2b0f63f485adc8ffe1823e8c243fe6b725c48a
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-09-02 19:26:23 +0530

    Implement tuple reversal using slicing

---
+++ reverse_tuple.py
@@ -0,0 +1,11 @@
+# Write a Python program to reverse the elements of a tuple.
+# create a tuple with values 10 20 30 Then
+# Use slicing with a negative step ([::-1]) to reverse.
+# Store the reversed tuple in a new variable.
+# Print the reversed tuple clearly.
+
+numbers = (10, 20, 30)
+
+reversed_tuple = numbers[::-1]
+
+print(reversed_tuple)
