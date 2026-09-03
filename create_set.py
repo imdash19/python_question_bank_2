8080a25f7bda4409142a7e7aff58fca423780512
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-09-03 19:09:04 +0530

    Refactor create_set.py to use comments for instructions

---
+++ create_set.py
@@ -0,0 +1,9 @@
+# Write a Python program to create a set containing numbers from 1 to 5.
+# The program should display the set elements.
+# It should also display the total number of elements present in the set.
+# Finally, the program must print the data type of the created set.
+# No input is required from the user.
+# The output should be displayed in a single readable line.
+
+my_set= {1, 2, 3, 4, 5}
+print(f'Set: {my_set}, Length: {len(my_set)}, Type: {type(my_set)}')
