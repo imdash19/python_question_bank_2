6a8655ef0b6b399160f575e073d5e93cd4c4e57f
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-09-09 20:59:01 +0530

    Implement lambda function to multiply by five

---
+++ lambda_multiply_by_five.py
@@ -0,0 +1,4 @@
+# Write a program that takes a single integer as input. Use a lambda function to multiply the number by a fixed constant value of 5. The input will be one integer, and the output should be the result of multiplying that number by 5.
+
+five= lambda x: x*5
+print(five(int(input())))
