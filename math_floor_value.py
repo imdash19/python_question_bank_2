c234e3cc26a8bb831dc56392bf47362001b6240b
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-09-12 20:21:46 +0530

    Refactor math_floor_value.py to include code

---
+++ math_floor_value.py
@@ -0,0 +1,8 @@
+# Write a Python program that calculates the floor value of a float number.
+# Input is provided by the user.Use math.floor() to compute the largest integer less than 
+# or equal to the number.The program should print the result as an integer.
+# Dot notation must be used to call the function.The program should handle
+# both positive and negative numbers.
+
+import math
+print(math.floor(float(input())))
